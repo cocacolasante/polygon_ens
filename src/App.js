1148c5d086a4f3f2d6f0cb830a6ec19600767d5b
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-01 21:23:31 -0400

    connect wallet button built

--- src/App.js
+++ src/App.js
@@ -1,38 +1,138 @@
-import twitterLogo from "./assets/twitter-logo.svg"
-import "./App.css"
 
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import twitterLogo from './assets/twitter-logo.svg';
+import {ethers} from "ethers";
 
+// Constants
 const TWITTER_HANDLE = 'cocacolasante';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+// Add the domain you will be minting
+const tld = '.ninja';
+const CONTRACT_ADDRESS = '0xeBE300BFB81779BD8C3fe821c04BC644852baD4c';
 
 const App = () => {
+	const [currentAccount, setCurrentAccount] = useState('');
+	// Add some state data propertie
+	const [domain, setDomain] = useState('');
+  const [record, setRecord] = useState('');
 
-  return (
-		<div className="App">
-			<div className="container">
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
 
-				<div className="header-container">
-					<header>
-            <div className="left">
-              <p className="title"> ABX Name Service</p>
-              <p className="subtitle">Your immortal domain name built on the blockchain!</p>
-            </div>
-					</header>
+      if (!ethereum) {
+        alert("Get MetaMask -> https://metamask.io/");
+        return;
+      }
+			
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+	const checkIfWalletIsConnected = async () => {
+		const { ethereum } = window;
+
+		if (!ethereum) {
+			console.log('Make sure you have metamask!');
+			return;
+		} else {
+			console.log('We have the ethereum object', ethereum);
+		}
+
+		const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log('Found an authorized account:', account);
+			setCurrentAccount(account);
+		} else {
+			console.log('No authorized account found');
+		}
+	};
+
+	// Render methods
+	const renderNotConnectedContainer = () => (
+		<div className="connect-wallet-container">
+			<img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja donut gif" />
+      {/* Call the connectWallet function we just wrote when the button is clicked */}
+			<button onClick={connectWallet} className="cta-button connect-wallet-button">
+				Connect Wallet
+			</button>
+		</div>
+	);
+	
+	// Form to enter domain name and data
+	const renderInputForm = () =>{
+		return (
+			<div className="form-container">
+				<div className="first-row">
+					<input
+						type="text"
+						value={domain}
+						placeholder='domain'
+						onChange={e => setDomain(e.target.value)}
+					/>
+					<p className='tld'> {tld} </p>
 				</div>
 
-        <div className="footer-container">
+				<input
+					type="text"
+					value={record}
+					placeholder='whats ur ninja power'
+					onChange={e => setRecord(e.target.value)}
+				/>
+
+				<div className="button-container">
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Mint
+					</button>  
+					<button className='cta-button mint-button' disabled={null} onClick={null}>
+						Set data
+					</button>  
+				</div>
+
+			</div>
+		);
+	}
+  
+	useEffect(() => {
+		checkIfWalletIsConnected();
+	}, []);
+
+	return (
+		<div className="App">
+			<div className="container">
+				<div className="header-container">
+					<header>
+						<div className="left">
+							<p className="title">ABX Name Service</p>
+							<p className="subtitle">Your Video Game Domain on the blockchain!</p>
+						</div>
+					</header>
+				</div>
+				
+				{!currentAccount && renderNotConnectedContainer()}
+				{/* Render the input form if an account is connected */}
+				{currentAccount && renderInputForm()}
+				
+				<div className="footer-container">
 					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
 					<a
 						className="footer-text"
 						href={TWITTER_LINK}
 						target="_blank"
 						rel="noreferrer"
-					>{`built by @${TWITTER_HANDLE}`}</a>
+					>{`built with @${TWITTER_HANDLE}`}</a>
 				</div>
 			</div>
 		</div>
 	);
-}
-
+};
 
 export default App;
