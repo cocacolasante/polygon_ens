e4a510d0fe24e9dacb87ee13804bbb68f7f4f348
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2022-09-02 21:30:45 -0400

    updating SC with modifiers and owner functions

--- scripts/run.js
+++ scripts/run.js
@@ -17,6 +17,28 @@ async function main() {
   const balance = await hre.ethers.provider.getBalance(domainContract.address);
   console.log("Contract Balance: ", hre.ethers.utils.formatEther(balance))
 
+  try{
+    txn = await domainContract.connect(randomAddress).connect(balance)
+    await txn.wait()
+    
+
+  }catch(error){
+    console.log("could not rob contract", error)
+  }
+
+  let ownerBalance = await hre.ethers.provider.getBalance(owner.address)
+  console.log("Balance of owners wallet: ", hre.ethers.utils.formatEther(ownerBalance))
+
+  txn = await domainContract.connect(owner).withdraw()
+  await txn.wait();
+
+  const contractBalance = await hre.ethers.provider.getBalance(domainContract.address);
+  ownerBalance = await hre.ethers.provider.getBalance(owner.address)
+
+  console.log("Contract balance after withdrawal:", hre.ethers.utils.formatEther(contractBalance));
+  console.log("Balance of owner after withdrawal:", hre.ethers.utils.formatEther(ownerBalance));
+
+
 
 }
 
--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import twitterLogo from './assets/twitter-logo.svg';
 import {ethers} from "ethers";
+import contractAbi from "./utils/contractAbi.json";
 
 // Constants
 const TWITTER_HANDLE = 'cocacolasante';
@@ -15,25 +16,25 @@ const App = () => {
 	const [currentAccount, setCurrentAccount] = useState('');
 	// Add some state data propertie
 	const [domain, setDomain] = useState('');
-  const [record, setRecord] = useState('');
+	const [record, setRecord] = useState('');
 
-  const connectWallet = async () => {
-    try {
-      const { ethereum } = window;
+	const connectWallet = async () => {
+	try {
+		const { ethereum } = window;
 
-      if (!ethereum) {
-        alert("Get MetaMask -> https://metamask.io/");
-        return;
-      }
+		if (!ethereum) {
+		alert("Get MetaMask -> https://metamask.io/");
+		return;
+		}
 			
-      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
-      
-      console.log("Connected", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (error) {
-      console.log(error)
-    }
-  }
+		const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+		
+		console.log("Connected", accounts[0]);
+		setCurrentAccount(accounts[0]);
+	} catch (error) {
+		console.log(error)
+	}
+	}
 
 	const checkIfWalletIsConnected = async () => {
 		const { ethereum } = window;
@@ -55,11 +56,57 @@ const App = () => {
 			console.log('No authorized account found');
 		}
 	};
+	const mintDomain = async () => {
+		// Don't run if the domain is empty
+		if (!domain) { return }
+		// Alert the user if the domain is too short
+		if (domain.length < 3) {
+			alert('Domain must be at least 3 characters long');
+			return;
+		}
+		// Calculate price based on length of domain (change this to match your contract)	
+		// 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+		const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+		console.log("Minting domain", domain, "with price", price);
+	  try {
+		const { ethereum } = window;
+		if (ethereum) {
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const signer = provider.getSigner();
+			const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+	
+			console.log("Going to pop wallet now to pay gas...")
+			let tx = await contract.register(domain, {value: ethers.utils.parseEther(price)});
+			// Wait for the transaction to be mined
+			const receipt = await tx.wait();
+
+			// Check if the transaction was successfully completed
+			if (receipt.status === 1) {
+				console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+				
+				// Set the record for the domain
+				tx = await contract.setRecord(domain, record);
+				await tx.wait();
+
+				console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
+				
+				setRecord('');
+				setDomain('');
+			}
+			else {
+				alert("Transaction failed! Please try again");
+			}
+	}
+	  }
+	  catch(error){
+		console.log(error);
+	  }
+	}
 
 	// Render methods
 	const renderNotConnectedContainer = () => (
 		<div className="connect-wallet-container">
-			<img src="https://media.giphy.com/media/3ohhwytHcusSCXXOUg/giphy.gif" alt="Ninja donut gif" />
+			<img src="https://media2.giphy.com/media/TTy5YmVmhmWhq/giphy.gif?cid=790b76113ea09d4deb5e91aeb9bbf922bd659972664a5cf4&rid=giphy.gif&ct=g" alt="Ninja donut gif" />
       {/* Call the connectWallet function we just wrote when the button is clicked */}
 			<button onClick={connectWallet} className="cta-button connect-wallet-button">
 				Connect Wallet
@@ -84,12 +131,12 @@ const App = () => {
 				<input
 					type="text"
 					value={record}
-					placeholder='whats ur ninja power'
+					placeholder='whats ur video game'
 					onChange={e => setRecord(e.target.value)}
 				/>
 
 				<div className="button-container">
-					<button className='cta-button mint-button' disabled={null} onClick={null}>
+					<button className='cta-button mint-button' disabled={null} onClick={mintDomain}>
 						Mint
 					</button>  
 					<button className='cta-button mint-button' disabled={null} onClick={null}>
